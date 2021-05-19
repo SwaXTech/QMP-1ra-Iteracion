a24802add873b59b73a3f46e685e83befd889b72
Alee <aleeherasimiuk@gmail.com>
2021-05-19 18:02:20 -0300

    Test de atuendo

--- TerceraIteracion/qmp/src/main/java/com/qmp/clima/BuenosAires.java
+++ TerceraIteracion/qmp/src/main/java/com/qmp/clima/BuenosAires.java
@@ -4,10 +4,12 @@ public class BuenosAires implements Ciudad{
 
   ServicioClima servicioClima = RepositorioServicios.getInstance().getServicioActual();
   
+  @Override
   public String probabilidadDeLluvia(){ 
     return servicioClima.probabilidadDeLluvias("Buenos Aires, Argentina");
   }
   
+  @Override
   public int temperaturaCelsius(){
     int tempFarenheit = servicioClima.temperatura("Buenos Aires, Argentina");
     return tempFarenheit - 32;
