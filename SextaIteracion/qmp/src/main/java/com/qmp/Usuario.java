660b2283fdd90cf56099b2b0e4034ed33e4ab0eb
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-06-08 16:40:34 -0300

    El usuario puede ser notificado

--- SextaIteracion/qmp/src/main/java/com/qmp/Usuario.java
+++ SextaIteracion/qmp/src/main/java/com/qmp/Usuario.java
@@ -5,14 +5,21 @@ import java.util.stream.Collectors;
 
 import com.qmp.guardarropas.Guardarropas;
 import com.qmp.guardarropas.Sugerencia;
+import com.qmp.otros_servicios.MailSender;
+import com.qmp.otros_servicios.NotificationService;
 
 public class Usuario {
   private List<Guardarropas> misGuardarropas;
   private List<Sugerencia> sugerencias;
 
-  public Usuario() {
+  private boolean recibirNotificaciones = true;
+  private boolean recibirMail = true;
+  private String email;
+
+  public Usuario(String email) {
     this.misGuardarropas = new ArrayList<>();
     this.sugerencias = new ArrayList<>();
+    this.email = email;
   }
 
   public void agregarGuardarropas(Guardarropas guardarropas){
@@ -33,6 +40,33 @@ public class Usuario {
 
   public List<Sugerencia> sugerencias(){
     return sugerencias;
+  }
+
+  public void dejarDeRecibirMails(){
+    recibirMail = false;
+  }
+
+
+  public void dejarDeRecibirNotificaciones(){
+    recibirNotificaciones = false;
+  }
+
+  public void comenzarARecibirMails(){
+    recibirMail = true;
+  }
+
+  public void comenzarARecibirNotificaciones(){
+    recibirNotificaciones = true;
+  }
+
+  public void serNotificado(NotificationService notiService, MailSender sender, String mensaje){
+    if(recibirNotificaciones){
+      notiService.notify(mensaje);
+    }
+
+    if(recibirMail){
+      sender.send(email, mensaje);
+    }
   }
 
   
